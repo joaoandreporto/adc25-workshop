fdccad9cb282cc54cb5b42069d971d673edd224d
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-11-06 08:48:49 +0000

    Added post-create setup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+sudo apt -y update
+sudo apt -y install libasound2-dev libjack-jackd2-dev \
+    ladspa-sdk \
+    libcurl4-openssl-dev  \
+    libfreetype-dev libfontconfig1-dev \
+    libx11-dev libxcomposite-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev \
+    libwebkit2gtk-4.1-dev \
+    libglu1-mesa-dev mesa-common-dev
+
+sudo apt -y install python3.12-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install .
